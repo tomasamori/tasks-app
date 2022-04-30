0e3269200e3d2e7126fa2e3443b068fbe8a2ab04
tomasamori <tomasamori15@gmail.com>
2022-04-30 02:33:50 -0300

    stage 3

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
+import {create} from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
+import path from "path";
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+
+const exphbs = create({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaultLayout:'main'
+});
+
+app.engine(".hbs", exphbs.engine);
+
+app.set("view engine", ".hbs");
+
+// routes
 app.use(indexRoutes);
 
 export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,11 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.render("index")
 });
 
 router.get("/about", (req, res) => {
-    res.send("about")
+    res.render("about")
+});
+
+router.get("/edit", (req, res) => {
+    res.render("edit")
 });
 
 export default router;
