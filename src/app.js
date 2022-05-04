31e347cfd740542d21167e6f7acda9c2400b7cb4
tomasamori <tomasamori15@gmail.com>
2022-05-04 19:21:43 -0300

    stage 6

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import {create} from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
 import path from "path";
+import morgan from "morgan";
 
 const app = express();
 
@@ -17,6 +18,10 @@ const exphbs = create({
 app.engine(".hbs", exphbs.engine);
 
 app.set("view engine", ".hbs");
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
 
 // routes
 app.use(indexRoutes);
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
+import Task from "../models/Task"
 
 const router = Router();
 
 router.get("/", (req, res) => {
     res.render("index")
 });
+
+router.post("/tasks/add", async (req, res) => {
+
+    const task = Task(req.body);
+
+    await task.save();
+
+    res.redirect("/");
+})
 
 router.get("/about", (req, res) => {
     res.render("about")
