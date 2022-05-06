8c474d77d65a806c1680bfc08e94da977ea3fb5f
tomasamori <tomasamori15@gmail.com>
2022-05-06 17:42:49 -0300

    stage 12

--- src/database.js
+++ src/database.js
@@ -6,7 +6,8 @@ import mongoose from "mongoose";
         const db = await mongoose.connect("mongodb://localhost/crud-mongo");
         console.log("DB connected to", db.connection.name);
     }
-    catch (error) {
+    catch (error)
+    {
         console.error(error);
     }
 })()
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,67 +1,26 @@
 import { Router } from "express";
-import Task from "../models/Task"
+
+import {
+    renderTasks,
+    createTask,
+    renderTaskEdit,
+    editTask,
+    deleteTask,
+    taskToggleDone
+} from "../controllers/tasks.controller";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", renderTasks);
 
-    const tasks = await Task.find().lean();
-    res.render("index", { tasks: tasks });
-});
+router.post("/tasks/add", createTask);
 
-router.post("/tasks/add", async (req, res) => {
-try{
-    const task = Task(req.body);
+router.get("/tasks/:id/toggleDone", taskToggleDone);
 
-    await task.save();
+router.get("/tasks/:id/edit", renderTaskEdit);
 
-    res.redirect("/");
-} catch (error) {
-    console.log(error);
-}
+router.post("/tasks/:id/edit", editTask);
 
-});
-
-router.get("/about", (req, res) => {
-    res.render("about");
-});
-
-router.get("/edit/:id", async (req, res) => {
-    try{
-        const task = await Task.findById(req.params.id).lean()
-
-        res.render("edit", { task });
-    } catch (error) {
-        console.log(error.message);
-    }
-});
-
-router.post("/edit/:id", async (req, res) => {
-    const {id} = req.params;
-
-    await Task.findByIdAndUpdate(id, req.body);
-
-    res.redirect("/");
-});
-
-router.get("/delete/:id", async (req, res) => {
-    const { id } = req.params;
-
-    await Task.findByIdAndDelete(id);
-
-    res.redirect("/");
-});
-
-router.get("/toggleDone/:id", async (req, res) => {
-    const { id } = req.params;
-
-    const task = await Task.findById(id);
-
-    task.done = !task.done;
-
-    await task.save();
-
-    res.redirect("/");
-});
+router.get("/tasks/:id/delete", deleteTask);
 
 export default router;
